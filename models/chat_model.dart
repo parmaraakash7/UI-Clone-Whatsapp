4f491998c368e55644bb3343ad71e5ecf2116083
Aakash Parmar <60379362+parmaraakash7@users.noreply.github.com>
2020-08-12 23:11:55 +0530

    Add files via upload
    Added chat_model file.

---
+++ models/chat_model.dart
@@ -0,0 +1,18 @@
+class ChatModel
+{
+	String name,message,time,avatarUrl;
+	ChatModel(this.name,this.message,this.time,this.avatarUrl);
+}
+
+List<ChatModel> data=[
+	new ChatModel("Saket Uchil","Hello.!!.","17.30",
+		"https://m.media-amazon.com/images/M/MV5BMjA3ODA0MDI4OF5BMl5BanBnXkFtZTgwMjEyNzI3MDE@._V1_UY317_CR131,0,214,317_AL_.jpg"),
+	new ChatModel("Chitrang","Hi chitrang.","7.30",
+		"https://pbs.twimg.com/profile_images/527856211559849984/t6PKbnm4_400x400.jpeg"),
+	new ChatModel("Karan","Hi karan.","19.30",
+		"https://qph.fs.quoracdn.net/main-qimg-b544b8a7fec58b0707f18e8fe68b7fad.webp"),
+	new ChatModel("Priyaank","Hi priyaank.","1.30",
+		"https://qph.fs.quoracdn.net/main-qimg-b544b8a7fec58b0707f18e8fe68b7fad.webp"),
+	new ChatModel("Jay Thard","Hi jay.","12.30",
+		"https://upload.wikimedia.org/wikipedia/commons/f/f7/Rohit_Sharma_November_2016_%28cropped%29.jpg"),
+];
